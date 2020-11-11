be2cb032fd9745d2e4c96c777444307fffb65427
cesarantonioastorga <cesarantonioastorga@users.noreply.github.com>
2020-11-11 19:20:09 -0300

    sahre code on github

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import data from "./data"
+
+function App() {
+  return (
+    <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">Ecommerce</a>
+                </div>
+                <div>
+                    <a href="/cart">Carrito</a>
+                    <a href="/singin">Registro</a>
+                </div>
+
+            </header>
+            <main>
+                <div>
+                    <div className="row center">
+                        {
+                            data.products.map(product => (
+                                <div key={product._id} className="card">
+                                    <a href={`/product/${product._id}`}>
+                                        <img className="medium" src={product.image} alt={product.name}></img>
+                                    </a>
+                                    <div className="card-body">
+                                        <a href={`/product/${product._id}`}>
+                                            <h2>{product.name}</h2>
+                                        </a>
+                                        <div className="rating"> 
+                                            <span>
+                                                <i className="fa fa-star"></i>
+                                            </span>
+                                            <span>
+                                                <i className="fa fa-star"></i>
+                                            </span>
+                                            <span>
+                                                <i className="fa fa-star"></i>
+                                            </span>
+                                            <span>
+                                                <i className="fa fa-star"></i>
+                                            </span>
+                                            <span>
+                                                <i className="fa fa-star-o"></i>
+                                            </span>
+                                        </div>
+                                        <div className="price">
+                                            ${product.price}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div> 
+            </main>
+            <footer className="row center">
+                All right reserved
+            </footer>
+        </div>
+  );
+}
+
+export default App;
